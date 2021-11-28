0ba81ebc2ab1e7234ec7beb764b816103e2343b7
Nicolas Vera <57852440+queondatodotranqui@users.noreply.github.com>
2021-11-27 21:43:28 -0300

    add signup form validation and submit

--- src/components/Login/SignupForm.jsx
+++ src/components/Login/SignupForm.jsx
@@ -1,31 +1,64 @@
 import * as React from 'react'
 
-import { OutlinedInput, Box, Button, MenuItem, Select } from '@mui/material'
+import { TextField, Box, Button, MenuItem, Select } from '@mui/material'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 import * as styles from './StylesLogin'
 
+const validationSchema = yup.object({
+    username:yup
+        .string('Enter your username')
+        .required('username is required'),
+    region:yup
+        .string('Enter your region')
+        .required('Region is required'),
+    email:yup
+        .string('Enter your email')
+        .email('Enter a valid email')
+        .required('Email is required'),
+    password:yup
+        .string('Enter your password')
+        .required('Password is required')
+})
+
 const SignUpForm = (props) =>{
 
-    const [value, setValue] = React.useState('')
-
-    const handleChange = (event) =>{
-        setValue(event.target.value)
-    }
+    const formik = useFormik({
+        initialValues:{
+            username:'',
+            region:'',
+            email:'',
+            password:''
+        },
+        validationSchema: validationSchema,
+        onSubmit:(values)=>{
+            console.log(values)
+        }
+    })
 
     return (
-        <form style={styles.FormCss}>
+        <form onSubmit={formik.handleSubmit} style={styles.FormCss}>
             <Box sx={{display:'flex', justifyContent:'space-between'}}>
-                <OutlinedInput
+                <TextField
                     sx={{backgroundColor:'whitesmoke', marginBottom:'1.8vh', width:'58%'}}
                     color="primary" 
                     placeholder="Username" 
-                    variant="outlined" 
+                    variant="outlined"
+                    id="username"
+                    name="username"
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    error={formik.touched.username && Boolean(formik.errors.username)}
                 />
                 <Select
-                    value={value}
                     displayEmpty
-                    onChange={handleChange}
                     sx={{backgroundColor:'whitesmoke', marginBottom:'1.8vh', width:'40%'}}
+                    id="region"
+                    name="region"
+                    value={formik.values.region}
+                    onChange={formik.handleChange}
+                    error={formik.touched.region && Boolean(formik.errors.region)}
                 >
                     <MenuItem value="">Region</MenuItem>
                     { props.regions.map((item, index) =>
@@ -34,18 +67,28 @@ const SignUpForm = (props) =>{
                 </Select>
             </Box>
             
-            <OutlinedInput
+            <TextField
                 sx={{backgroundColor:'whitesmoke', marginBottom:'1.8vh'}}
                 color="primary" 
                 placeholder="Email" 
-                variant="outlined" 
+                variant="outlined"
+                id="email"
+                name="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
             />
-            <OutlinedInput
+            <TextField
                 sx={{backgroundColor:'whitesmoke'}}
                 type="password"
                 color="primary" 
                 placeholder="Password" 
                 variant="outlined" 
+                id="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
             />
             <Box component="div" sx={styles.ButtonBox}>
                 <Button 
@@ -59,8 +102,8 @@ const SignUpForm = (props) =>{
                 <Button 
                     sx={{backgroundColor:'#1C9984', textTransform:'none'}} 
                     variant="contained" 
-                    onClick={console.log('y si')} 
                     size="large"
+                    type="submit"
                 >
                     Sign up
                 </Button>
